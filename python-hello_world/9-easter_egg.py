1d9790f0351bb3a71d4ff7686962e7a072e93de2
Diano ANDRIANTSALAMA <diano@angetech.com>
2026-05-08 14:35:50 +0400

    import the zen of python

--- python-hello_world/9-easter_egg.py
+++ python-hello_world/9-easter_egg.py
@@ -1,5 +1,2 @@
-title = "The Zen of Python, by Tim Peters \n\n"
-
-l1 = "Beautiful is better than ugly.\n"
-l2 = "Explicit is better than implicit.\n"
-l2 = "Simple is better than complex.\n"
+#!/usr/bin/python3
+import this
