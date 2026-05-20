ab60e0873fa76eccf56802fcfe438fe174a10692
Diano ANDRIANTSALAMA <diano.faniry@gmail.com>
2026-05-20 15:21:26 +0400

    capitalize

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    complete_str = ""
+    for char in str:
+        complete_str += char.capitalize()
+
+    return print(complete_str)
+
+
+uppercase("test")
