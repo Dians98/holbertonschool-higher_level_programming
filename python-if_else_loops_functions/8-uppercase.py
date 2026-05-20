7b56d6cfbefd78f40872ed330feea5aca59d0bc4
Diano ANDRIANTSALAMA <diano.faniry@gmail.com>
2026-05-20 15:24:09 +0400

    co

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    complete_str = ""
     for char in str:
-        complete_str += char.capitalize()
-
-    return complete_str
+        if 97 <= ord(char) <= 122:
+            print("{}".format(chr(ord(char) - 32)), end="")
+        else:
+            print("{}".format(char), end="")
+    print()
