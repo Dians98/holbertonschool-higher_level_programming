e4dbf7aba4772e914b6ddaf25264cfc4792266c6
Diano ANDRIANTSALAMA <diano.faniry@gmail.com>
2026-05-21 15:26:01 +0400

    tuples

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    t1, t2 = tuple_a, tuple_b
+    t1_len = len(t1)
+    t2_len = len(t2)
+
+    res = tuple(((t1[i] if i < t1_len else 0) + (t2[i] if i < t2_len else 0))
+                for i in range(2))
