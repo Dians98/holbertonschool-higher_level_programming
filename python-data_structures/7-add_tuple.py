37eb294b3d5895da762362ac2752df8978ea8709
Diano ANDRIANTSALAMA <diano.faniry@gmail.com>
2026-05-21 15:26:59 +0400

    a_plus_b

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -4,5 +4,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     t1_len = len(t1)
     t2_len = len(t2)
 
-    res = tuple(((t1[i] if i < t1_len else 0) + (t2[i] if i < t2_len else 0))
-                for i in range(2))
+    a_plus_b = tuple(((t1[i] if i < t1_len else 0) + (t2[i] if i < t2_len else 0))
+                     for i in range(2))
+    print("{:d}".format(a_plus_b))
