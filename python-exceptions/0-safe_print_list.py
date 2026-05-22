137d324aed46ee1e689c05e5a30b160c67748886
Diano ANDRIANTSALAMA <diano.faniry@gmail.com>
2026-05-22 06:33:23 +0400

    dsqdqsd

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             nb_print += 1
-        except IndexError:
+        except Exception as e:
             break
     print()
     return nb_print
