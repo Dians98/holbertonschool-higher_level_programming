9e581fd4b941d3f35b20ba234d2bd18230b787a8
Diano ANDRIANTSALAMA <diano.faniry@gmail.com>
2026-05-22 06:30:09 +0400

    rea

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,9 +6,3 @@ def safe_print_list(my_list=[], x=0):
         pass
     except Exception as e:
         raise e
-
-
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
