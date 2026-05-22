596ae59bf438908bc42ab394bc75ac254a766f8c
Diano ANDRIANTSALAMA <diano.faniry@gmail.com>
2026-05-22 14:03:30 +0400

    comment module

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+    This Module add the sum of two integer
+"""
+
+
 def add_integer(a, b=98):
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
