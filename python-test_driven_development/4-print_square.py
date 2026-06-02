f7005ae5f31481c24e4e9dcd45a62026a66a8410
Diano ANDRIANTSALAMA <diano.faniry@gmail.com>
2026-06-02 10:46:11 +0400

    dsqdqs

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -6,8 +6,10 @@ This module provide a function that prints a square with the character #
 
 def print_square(size):
     """
-
+    This function prints the square of a siez with #
     """
+    if isinstance(size, float):
+        raise TypeError("size must be an integer")
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
@@ -15,18 +17,3 @@ def print_square(size):
 
     for i in range(size):
         print("#" * size)
-
-
-print_square(4)
-print("")
-print_square(10)
-print("")
-print_square(0)
-print("")
-print_square(1)
-print("")
-try:
-    print_square(-1)
-except Exception as e:
-    print(e)
-print("")
