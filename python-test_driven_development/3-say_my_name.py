f2fa463c196e9cfd5eaf6a8ec9f1403ebc29a6c4
Diano ANDRIANTSALAMA <diano.faniry@gmail.com>
2026-06-02 10:22:11 +0400

    dqdsqdqs

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -9,8 +9,9 @@ def say_my_name(first_name, last_name=""):
         Print full name
         Raise an error if last_name or first_name is not a string
     """
-    if not isinstance(first_name, str) or not isinstance(last_name, str):
-        raise TypeError(
-            "first_name must be a string or last_name must be a string")
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
 
     print("My name is {} {}".format(first_name, last_name))
