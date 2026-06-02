1e1907341c4fd310b5d95a9714baca7941c93150
Diano ANDRIANTSALAMA <diano.faniry@gmail.com>
2026-06-02 10:12:50 +0400

    dqsd

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+This module provide a function for saying name
+"""
+
+
 def say_my_name(first_name, last_name):
+    """
+        Print full name
+        Raise an error if last_name or first_name is not a string
+    """
     if not isinstance(first_name, str) or not isinstance(last_name, str):
         raise TypeError(
             "first_name must be a string or last_name must be a string")
