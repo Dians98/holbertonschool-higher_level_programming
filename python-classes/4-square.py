4a10fdb899bc7e2c60f373757eedf78dee12c483
Diano ANDRIANTSALAMA <diano.faniry@gmail.com>
2026-06-04 20:08:57 +0400

    dsqdqd

---
+++ python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" 
+This module is for an empty class that define square
+"""
+
+
+class Square:
+    """
+    Class Square
+    """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+            pass
+        if size < 0:
+            raise ValueError("size must be >= 0")
+            pass
+        self.__size = size
+        pass
+
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+            pass
+        if value < 0:
+            raise ValueError("size must be >= 0")
+            pass
+        self.__size = value
+        pass
+
+    def area(self):
+        return self.__size**2
+    pass
