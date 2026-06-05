ae920f4ea3e4c2608a2c504b426707af30047279
Diano ANDRIANTSALAMA <diano.faniry@gmail.com>
2026-06-05 12:18:36 +0400

    dqsdqsdqs

---
+++ python-input_output/11-student.py
@@ -0,0 +1,26 @@
+class Student:
+    """Defines a student by their first name, last name, and age."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance.
+
+        If attrs is a list of strings, only attribute names contained
+        in this list must be retrieved. Otherwise, all attributes are retrieved.
+        """
+        if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance using a dictionary.
+
+        Assumes json is a dictionary where keys are attribute names and
+        values are the corresponding values to update.
+        """
+        self.__dict__.update(json)
---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,4 @@
+def append_write(filename="", text=""):
+    """Appends a string to the end of a UTF-8 text file and returns the number of characters added."""
+    with open(filename, mode="a", encoding="utf-8") as file:
+        return file.write(text)
