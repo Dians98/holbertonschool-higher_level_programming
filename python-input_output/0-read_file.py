6977e4af37c7a15839f8e9296d0a94306f82ffa3
Diano ANDRIANTSALAMA <diano.faniry@gmail.com>
2026-06-05 12:47:57 +0400

    dsqdqs

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This module is used for input/output exercices
+"""
+
+
 def read_file(filename=""):
     """Reads a UTF-8 text file and prints its content to stdout."""
     with open(filename, mode="r", encoding="utf-8") as file:
