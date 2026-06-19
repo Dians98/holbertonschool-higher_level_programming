1bcaa208f227db96dc4e1bdf4fb4063cce582ae7
Diano ANDRIANTSALAMA <diano.faniry@gmail.com>
2026-06-19 10:52:59 +0400

    dsqdqsd

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+MY FILTER STATE.
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # db = MySQLdb.connect(
+    #     host="localhost",
+    #     port=3308,
+    #     user="root",
+    #     passwd="root",
+    #     db="hbtn_0e_0_usa"
+    # )
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+
+    )
+    state_name = sys.argv[4]
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY states.id ASC",
+        (state_name + '%',)
+    )
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
