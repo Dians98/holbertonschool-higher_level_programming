a84a5673aca047c85f207f73086e1aea59ee628f
Diano ANDRIANTSALAMA <diano.faniry@gmail.com>
2026-06-19 09:27:31 +0400

    dsqdq

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- CITIES
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE cities IF NOT EXISTS(
+    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    state_id INT NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
