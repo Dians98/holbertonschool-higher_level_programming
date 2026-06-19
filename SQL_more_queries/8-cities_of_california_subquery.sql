ca428becdf068acadc28a7f2197e6734a529146a
Diano ANDRIANTSALAMA <diano.faniry@gmail.com>
2026-06-19 09:39:33 +0400

    dsqdq

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,6 @@
 -- CITIES OF CALIFORNIA
 
-USE hbtn_0d_usa;
-
-SELECT * FROM cities WHERE state_id = (
+SELECT id, name  FROM cities WHERE state_id = (
     SELECT id FROM states WHERE name = 'California'
     )
     ORDER BY id ASC;
