d864ef0e3702ea48fb0a6b144ac48ea5f4171df4
Diano ANDRIANTSALAMA <diano.faniry@gmail.com>
2026-06-19 10:46:00 +0400

    dsqdqs

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -16,9 +16,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.args[1],
-        passwd=sys.args[2],
-        db=sys.args[3]
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
 
     cursor = db.cursor()
